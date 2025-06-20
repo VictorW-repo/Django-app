f029fb1dc6623f44d52155c997e60a73b10c962f
Victor Wang <wangruijie0802@gmail.com>
2025-06-20 10:52:50 -0400

    feat: Add ASGI configuration for async support

--- iot_project/asgi.py
+++ iot_project/asgi.py
@@ -1,16 +1,11 @@
 """
-ASGI config for iot_parser project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+ASGI config for iot_project.
 """
 
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "iot_parser.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'iot_project.settings')
 
 application = get_asgi_application()
